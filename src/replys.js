d94b98330e76c5844e667e1a4810d661a240582a
RedTn <redmond.tran@gmail.com>
2020-12-19 19:28:13 -0800

    feat: reply john

--- src/replys.js
+++ src/replys.js
@@ -2,6 +2,9 @@ const main = (message) => {
     if (message.content === 'who is the best kiddo?') {
         message.channel.send('Naynay!');
     }
+    if (message.content === 'john is gay') {
+        message.channel.send('agreed');
+    }
 };
 
 module.exports = main;
