131d7048611720683a637894706a61bf01b758f3
Redmond Tran <redmond.tran@gmail.com>
2021-01-16 15:30:02 -0800

    fix: disable gloom report

--- src/poller.ts
+++ src/poller.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Discord from 'discord.js';
 import IState from './typings/IState';
 import { watchGameMembers } from './watch';
 
 export default (client: Discord.Client, state: IState): void => {
     setInterval(() => {
-        watchGameMembers(client, state);
+        // watchGameMembers(client, state);
     }, 1500);
 };
